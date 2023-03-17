4d8801d29a5f55c65bf1f2cc1583af44f638f56f
unknown <ajaydives3@gmail.com>
2023-03-17 14:42:13 -0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { logo } from './assets';
+import { Home, CreatePost } from './pages';
+import './pages/bg.css'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <header className='h-20 background-animate bg-gradient-to-r from-blue-400 via-purple-400 to-blue-500 w-full flex justify-between items-center bg-[#C25188] sm:px-8 py-4 border-b border-b-[#e6ebf4]'>
+          <Link to='/'>
+            <img src={logo} alt="logo" className="w-10 mx-4 object-contain" />
+          </Link>
+
+          <Link to='/create-post'
+            className='shadow-md font-inter font-medium bg-blue-600 text-white px-4 py-2 mx-3 rounded-md hover:bg-blue-800'>
+            Generate
+          </Link>
+        </header>
+
+        <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/create-post' element={<CreatePost />}/>
+          </Routes>
+
+        </main>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../pages/bg.css'
+
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => (
+  <div className='flex flex-col'>
+
+    <div className="mb-1 flex items-center gap-2 rounded-md">
+       <label
+        htmlFor={name}
+      >
+        {labelName}
+      </label>
+      {isSurpriseMe && (
+        <button
+          type="button"
+          onClick={handleSurpriseMe}
+          className="bg-gradient-to-r from-blue-400 via-purple-400 to-blue-500 background-animate font-semibold text-xs  py-1 px-2 rounded-[5px] text-white "
+        > Surprise me </button>
+      )}
+    </div>
+
+    <input
+      type={type}
+      id={name}
+      name={name}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      required
+    />
+
+
+  </div>
+);
+
+export default FormField;
