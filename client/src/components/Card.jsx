8c30437a5dbbf875ba68e9d9fe7b6f6441ba8716
unknown <ajaydives3@gmail.com>
2023-03-18 12:01:23 -0700

    fix ui

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({ _id, name, prompt, photo }) => (
       src={photo}
       alt={prompt}
     />
-    <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-2 rounded-md bg-opacity-50 backdrop-filter backdrop-blur-xl">
+    <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-2 rounded-md bg-opacity-40 backdrop-filter backdrop-blur-xl">
       <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
 
       <div className="mt-5 flex justify-between items-center gap-2">
@@ -18,8 +18,8 @@ const Card = ({ _id, name, prompt, photo }) => (
           <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">{name[0]}</div>
           <p className="text-white text-sm">{name}</p>
         </div>
-        <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none flex flex-row text-white">
-          download <img src={download} alt="download" className="w-6 h-6 object-contain invert" />
+        <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none flex flex-row text-white text-[0.625rem]">
+          Download <img src={download} alt="download" className="w-6 h-6 object-contain invert ml-1" />
         </button>
       </div>
     </div>
